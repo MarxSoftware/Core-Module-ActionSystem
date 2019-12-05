72fac436adb00409651458de2558b248d1d16fa4
Thorsten Marx <thmarx@gmx.net>
2019-12-05 11:43:19 +0100

    add cache

--- module/src/main/java/com/thorstenmarx/webtools/core/modules/actionsystem/module/SegmentRestUserInformationExtension.java
+++ module/src/main/java/com/thorstenmarx/webtools/core/modules/actionsystem/module/SegmentRestUserInformationExtension.java
@@ -7,7 +7,9 @@ import com.thorstenmarx.webtools.api.cache.CacheLayer;
 import com.thorstenmarx.webtools.api.datalayer.SegmentData;
 import com.thorstenmarx.webtools.api.extensions.RestUserInformationExtension;
 import com.thorstenmarx.webtools.core.modules.actionsystem.CacheKey;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import javax.inject.Inject;
 
 /**
@@ -17,24 +19,38 @@ import javax.inject.Inject;
 @Extension(RestUserInformationExtension.class)
 public class SegmentRestUserInformationExtension extends RestUserInformationExtension {
 
+	@Inject
+	private CacheLayer cachelayer;
+
+	private final String SEGMENT_DATA_KEY = "segmentdata_%s";
+
 	@Override
 	public JSONObject getUserInformation(String userid) {
 		JSONObject segments = new JSONObject();
-		
+
 //		List<SegmentData> segmentList = CoreModuleActionSystemExtensionImpl.userSegmentStore.get(userid);
-		List<SegmentData> segmentList = CoreModuleActionSystemExtensionImpl.userSegmentGenerator.generate(userid);
+		final String cacheKey = String.format(SEGMENT_DATA_KEY, userid);
+
+		ArrayList<SegmentData> segmentList = null;
+		if (cachelayer.exists(cacheKey)) {
+			segmentList = cachelayer.get(cacheKey, ArrayList.class).get();
+		} else {
+			segmentList = (ArrayList<SegmentData>) CoreModuleActionSystemExtensionImpl.userSegmentGenerator.generate(userid);
+			cachelayer.add(cacheKey, segmentList, 30, TimeUnit.SECONDS);
+		}
+		
 		JSONArray segmentArray = new JSONArray();
 		segmentList.forEach((sd) -> {
 			JSONObject segment = new JSONObject();
 			segment.put("id", sd.getSegment().id);
 			segment.put("name", sd.getSegment().name);
 			segment.put("wpid", sd.getSegment().wpid);
-			
+
 			segmentArray.add(segment);
 		});
-		
+
 		segments.put("segments", segmentArray);
-		
+
 		return segments;
 	}
 
@@ -51,5 +67,5 @@ public class SegmentRestUserInformationExtension extends RestUserInformationExte
 	@Override
 	public void init() {
 	}
-	
+
 }
